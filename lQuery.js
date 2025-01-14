787b08f32ca90ce5a87f4a9af6314db83c3a73f6
Igor Anzheyevskiy Zakharyk <igor_lifeking@hotmail.com>
2025-01-14 11:26:05 +0000

    feat: added methods to manipulate or replace elements

--- lQuery.js
+++ lQuery.js
@@ -25,6 +25,19 @@ class LQueryDOM {
     }
 
     /**
+     * **getHTML()** - Returns outer html of current element
+     * 
+     * ### Parameters:
+     * 
+     * 
+     * ### Returns
+     * - Returns outer html of current element
+    */
+    getHTML() {
+        return this.dom.map(e => e.outerHTML).join("")
+    }
+
+    /**
      * **html()** - Get or set the html contents of the element/s.
      * 
      * ### Parameters:
@@ -159,13 +172,17 @@ class LQueryDOM {
      *  **append()** - Append html to element/s
      * 
      * ### Parameters:
-     * - "html" (string): html or text to append
+     * - "html" (string | element): html or text to append
      * 
      * ### Returns:
      * - Returns instance of "this" for method chaining when setting the value.
      */
     append(html) {
-        this.dom.forEach(e => e.insertAdjacentHTML("beforeend", html))
+        if (typeof html === "string") {
+            this.dom.forEach(e => e.insertAdjacentHTML("beforeend", html))
+        } else if (html instanceof HTMLElement) {
+            this.dom.forEach(e => e.appendChild(html))
+        }
         return this
     }
 
@@ -173,13 +190,17 @@ class LQueryDOM {
      *  **prepend()** - Prepend html to element/s
      * 
      * ### Parameters:
-     * - "html" (string): html or text to prepend
+     * - "html" (string | element): html or text to prepend
      * 
      * ### Returns:
      * - Returns instance of "this" for method chaining when setting the value.
      */
     prepend(html) {
-        this.dom.forEach(e => e.insertAdjacentHTML("afterbegin", html))
+        if (typeof html === "string") {
+            this.dom.forEach(e => e.insertAdjacentHTML("afterbegin", html))
+        } else if (html instanceof HTMLElement) {
+            this.dom.forEach(e => e.insertBefore(html, e.firstChild))
+        }
         return this
     }
 
@@ -187,13 +208,17 @@ class LQueryDOM {
      *  **after()** - Insert html after the element/s
      * 
      * ### Parameters:
-     * - "html" (string): html or text to insert
+     * - "html" (string | element): html or text to insert
      * 
      * ### Returns:
      * - Returns instance of "this" for method chaining when setting the value.
      */
     after(html) {
-        this.dom.forEach(e => e.insertAdjacentHTML("afterend", html))
+        if (typeof html === "string") {
+            this.dom.forEach(e => e.insertAdjacentHTML("afterend", html))
+        } else if (html instanceof HTMLElement) {
+            this.dom.forEach(e => e.parentNode.insertBefore(html, e.nextSibling))
+        }
         return this
     }
 
@@ -201,13 +226,120 @@ class LQueryDOM {
      *  **before()** - Insert html before the element/s
      * 
      * ### Parameters:
-     * - "html" (string): html or text to insert
+     * - "html" (string | element): html or text to insert
      * 
      * ### Returns:
      * - Returns instance of "this" for method chaining when setting the value.
      */
     before(html) {
-        this.dom.forEach(e => e.insertAdjacentHTML("beforebegin", html))
+        if (typeof html === "string") {
+            this.dom.forEach(e => e.insertAdjacentHTML("beforebegin", html))
+        } else if (html instanceof HTMLElement) {
+            this.dom.forEach(e => e.parentNode.insertBefore(html, e))
+        }
+        return this
+    }
+
+    /**
+     *  **remove()** - Removes element/s from DOM
+     * 
+     * ### Parameters:
+     * - "selector" (string) [optional]: When provided, removes matching element's childs.
+     * 
+     * ### Returns:
+     * - void.
+     */
+    remove(selector) {
+        if (selector) {
+            this.dom.forEach(e => e.querySelectorAll(selector).forEach(x => x.remove()))
+        } else {
+            this.dom.forEach(e => e.remove())
+        }
+    }
+
+    /**
+     *  **empty()** - Removes all child elements
+     * 
+     * ### Parameters:
+     * Method does not accept parameters
+     * 
+     * ### Returns:
+     * - Returns instance of "this" for method chaining
+     */
+    empty() {
+        this.dom.forEach(e => e.innerHTML = "")
+        return this
+    }
+
+    /**
+     *  **wrap()** - Wraps current element with new DOM element
+     * 
+     * ### Parameters:
+     * - "element" (string | Element ): Wrapper element, either a string representation or DOM element.
+     * 
+     * ### Returns:
+     * - Returns instance of "this" for method chaining
+     * 
+     * ### Example:
+     * $(".important").wrap("p")
+     */
+    wrap(element) {
+        if (typeof element === "string") {
+            const newElement = document.createElement(element)
+            this.before(newElement)
+            const newLquery = new LQueryDOM("null")
+            newLquery.dom = [newElement]
+            this.dom.forEach(e => {
+                newLquery.append(e)
+            })
+        } else if (element instanceof Document) {
+            this.dom.forEach(e => {
+                element.appendChild(e)
+            })
+        }
+        return this
+    }
+
+    /**
+     *  **clone()** - Clones current element/s
+     * 
+     * ### Parameters:
+     * - "cloneEvents" (bool) [optional] - When set to true, also copies all events
+     * 
+     * ### Returns:
+     * - Returns a cloned instance of the selected element/s
+     */
+    clone(cloneEvents = false) {
+        const newLQueryDom = new LQueryDOM("null") // empty selector since we manually will asing this.dom
+        newLQueryDom.dom = this.dom.map(e => e.cloneNode(true))
+        if (cloneEvents) {
+            this.dom.forEach((e, i) => {
+                events.copyEvents(e, newLQueryDom.dom[i])
+            })
+        }
+        return newLQueryDom
+    }
+
+    /**
+     *  **replaceWith()** - Replaces currently selected element/s
+     * 
+     * ### Parameters:
+     * - "newContent" (string, Element, LQuery) - Content that will replace current element*s
+     * 
+     * ### Returns:
+     * - Returns instance of removed element.
+     */
+    replaceWith(newContent) {
+        if (newContent instanceof LQueryDOM) {
+            newContent.dom.forEach(e => {
+                this.after(e)
+            })
+            this.remove()
+        } else {
+            this.dom.forEach(e => {
+                e.replaceWith(newContent)
+            })
+        }
         return this
     }
 
@@ -222,7 +354,7 @@ class LQueryDOM {
      * - Returns instance of "this" for method chaining.
      */
     on(type, fn) {
-        this.dom.forEach(e => e.addEventListener(type, fn))
+        this.dom.forEach(e => events.addEvent(e, type, fn))
         return this
     }
 
@@ -237,7 +369,7 @@ class LQueryDOM {
      * - Returns instance of "this" for method chaining.
      */
     off(type, fn) {
-        this.dom.forEach(e => e.removeEventListener(type, fn))
+        this.dom.forEach(e => events.removeEvent(e, type, fn))
         return this
     }
 
@@ -252,7 +384,7 @@ class LQueryDOM {
      */
     click(fn) {
         if (fn) {
-            this.dom.forEach(e => e.addEventListener("click", fn))
+            this.on("click", fn)
         } else {
             this.dom.forEach(e => e.dispatchEvent(new Event("click")))
         }
@@ -270,7 +402,7 @@ class LQueryDOM {
      */
     dblclick(fn) {
         if (fn) {
-            this.dom.forEach(e => e.addEventListener("dblclick", fn))
+            this.on("dblclick", fn)
         } else {
             this.dom.forEach(e => e.dispatchEvent(new Event("dblclick")))
         }
@@ -288,12 +420,8 @@ class LQueryDOM {
      * - Returns instance of "this" for method chaining.
      */
     hover(fnOnHover, fnOnLeave) {
-        this.dom.forEach(e => {
-            e.addEventListener("mouseenter", fnOnHover)
-            if (fnOnLeave) {
-                e.addEventListener("mouseleave", fnOnLeave)
-            }
-        })
+        this.on("mouseenter", fnOnHover)
+        if (fnOnLeave) this.on("mouseleave", fnOnLeave)
         return this
     }
 
@@ -308,7 +436,7 @@ class LQueryDOM {
      */
     focus(fn) {
         if (fn) {
-            this.dom.forEach(e => e.addEventListener("focus", fn))
+            this.on("focus", fn)
         }
         else {
             this.dom.forEach(e => e.dispatchEvent(new Event("focus")))
@@ -327,7 +455,7 @@ class LQueryDOM {
      */
     blur(fn) {
         if (fn) {
-            this.dom.forEach(e => e.addEventListener("blur", fn))
+            this.on("blur", fn)
         } else {
             this.dom.forEach(e => e.dispatchEvent(new Event("blur")))
         }
@@ -349,3 +477,49 @@ class LQueryDOM {
         return this
     }
 }
+
+class eventTracker {
+    constructor() {
+        this.events = new Map() // Map gives better performance compared to Array.
+    }
+
+    addEvent(element, type, fn) {
+        // Create entry if it does not exist.
+        if (!this.events.has(element)) {
+            this.events.set(element, [])
+        }
+        // Asign event object to entry value.
+        this.events.get(element).push({ type: type, function: fn })
+        // Add event listener
+        element.addEventListener(type, fn)
+    }
+
+    removeEvent(element, type, fn) {
+        if (this.events.has(element)) {
+            // Get all entry values that does NOT contain the event object to delete.
+            const eventsArray = this.events.get(element).filter(event =>
+                !(event.type === type && event.function === fn)
+            )
+            // If result array is emtpy delete entry, in other case, replace current value with the arr that does not contain the deleted event.
+            if (eventsArray.length > 0) {
+                // Replace value.
+                this.events.set(element, eventsArray)
+            } else {
+                // Delete entry.
+                this.events.delete(element)
+            }
+            // Remove event listener.
+            element.removeEventListener(type, fn)
+        }
+    }
+
+    copyEvents(originalElement, newElement) {
+        if (this.events.has(originalElement)) {
+            const originalEvents = this.events.get(originalElement)
+            originalEvents.forEach(event => {
+                this.addEvent(newElement, event.type, event.function)
+            })
+        }
+    }
+}
+const events = new eventTracker()
